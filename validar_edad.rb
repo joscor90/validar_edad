9a4aa424126f387c9b54a72a1f05467375ce998d
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-26 13:20:11 -0400

    Added calling the method 3 times

--- validar_edad.rb
+++ validar_edad.rb
@@ -1,9 +1,13 @@
 #Method
 def validar_edad(edad)
-
     if edad >= 18
     puts "es mayor"
     else
     puts "es menor"
     end
 end
+
+#Calling validar edad
+validar_edad(rand(37))
+validar_edad(rand(37))
+validar_edad(rand(37))
